6f7cfff2029b309aea3fc47ebf57c0c60b883e64
SilvaHomeNigra <carolin.albus@live.de>
2024-01-06 19:07:03 +0100

    fix

--- Semesterarbeit/script.js
+++ Semesterarbeit/script.js
@@ -7,30 +7,30 @@ let fuchsElement = document.getElementById('Fuchs').innerHTML;
 let rueckseitenTexte = [
     "Hey ich bin Hops und ein Hase. Ich mag es sehr gestreichelt zu werden und wenn ich müde bin Kuschel ich mich gerne ins Stroh. Am liebsten esse ich Löwenzahn das hört sich ziemlich lustig an." ,
     "Hallöchen ich bin Gusti Gans. Am liebsten Watschel ich durch die Gegend und spiele mit meinen Freunden. Außerdem kann ich ganz laut schnattern. Weißt du wie das klingt?" , 
-    "Servus, ich bin Fredi Fuchs. Ich gehe gerne auf Streifzug durch den Wald. Ich bin vor allem in der nacht unterwegs und kann ganz laut bellen. Hast du das Schoneinmal gehört."
+    "Servus, ich bin Fredi Fuchs. Ich gehe gerne auf Streifzug durch den Wald. Ich bin vor allem in der nacht unterwegs und kann ganz laut bellen. Hast du das Schoneinmal gehört.",
 ];
 
-// Audiodateien für die Tiere
-let audioDateien = [
-  "pfad/zur/hase_audio.mp3",
-  "pfad/zur/gans_audio.mp3",
-  "pfad/zur/fuchs_audio.mp3" 
-];
 
+// Funktion AnimalContent - Anzeigen des Textes und abspielen der Audio
+function AnimalContent() {
+    haseElement.addEventListener ('click' , function  (rueckseitenTexte [0]))
+    haseElement = rueckseitenTexte [0]
+    gansElement = rueckseitenTexte [1]
+    fuchsElement = rueckseitenTexte [2]
+}
 // Funktion, die auf Klick reagiert
-haseElement.addEventListener('click', function() {
-    AnimalContent(0);
-  });
+//haseElement.addEventListener('click', function() {
+ //    AnimalContent;
+  //});
   
-  gansElement.addEventListener('click', function() {
-    AnimalContent(1);
-  });
+  //gansElement.addEventListener('click', function() {
+   // AnimalContent;
+ // });
   
-  fuchsElement.addEventListener('click', function() {
-    AnimalContent(2);
-  });
+  //fuchsElement.addEventListener('click', function() {
+   // AnimalContent;
+  //});
 
 
 
 
-function AnimalContent = 
